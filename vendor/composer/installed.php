c7fd030b8c52dfbbc9a0d8d25998fac19c6e81a5
cp6 <cp6@users.noreply.github.com>
2021-08-14 18:45:21 +1000

    v1.1
    v1.1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c8ed550a3d3b76227555e2d2670ac0e2b1dbbf7b',
+        'name' => 'corbpie/twitch-api-class',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'corbpie/twitch-api-class' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c8ed550a3d3b76227555e2d2670ac0e2b1dbbf7b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
